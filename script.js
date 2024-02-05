5948ce9293c3f6779f53531e87d5fbc4838717da
lazarodanie83 <ldss158@gmail.com>
2024-02-05 14:15:04 -0300

    4º aula(finalizando a 4ª aula) imersão alura frontend

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
-const serchInput = document.getElementById('search-input');
-const resultArtist = document.getElementById('result-artist');
-const resultPlaylist = document.getElementById('result-playlist');
+const searchInput = document.getElementById('search-input');
+const resultArtist = document.getElementById("result-artist");
+const resultPlaylist = document.getElementById('result-playlists');
 
-function requestApi(searchTerm){
-    
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResults(result))
 }
 
-document.addEventListener('input', () => {
+function displayResults(result) {
+    resultPlaylist.classList.add("hidden")
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+    });
+
+    resultArtist.classList.remove('hidden');
+}
+
+document.addEventListener('input', function () {
     const searchTerm = searchInput.value.toLowerCase();
-    if (searchTerm === ''){
+    if (searchTerm === '') {
         resultPlaylist.classList.add('hidden');
-        resultArtits.classList.remove('hidden');
-        return;
-    }      
+        resultArtist.classList.remove('hidden');
+        return
+    }
+    
+    requestApi(searchTerm);
 })
